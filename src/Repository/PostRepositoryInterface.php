47c0a7df60096f80dd86c923c196284b6b157ca9
MaximilanL <maxlatyshevich@gmail.com>
2021-06-14 17:00:36 +0300

    fx

--- src/Repository/PostRepositoryInterface.php
+++ src/Repository/PostRepositoryInterface.php
@@ -4,6 +4,10 @@
 namespace App\Repository;
 
 
+use App\Entity\Post;
+use phpDocumentor\Reflection\Types\Object_;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 interface PostRepositoryInterface
 {
     /**
@@ -12,7 +16,28 @@ interface PostRepositoryInterface
     public function getAllPost(): array;
 
     /**
-     * @return object
+     * @param int $postId
+     * @return Post
      */
-    public function getOnePost(): object;
+    public function getOnePost(int $postId): object;
+
+    /**
+     * @param Post $post
+     * @param UploadedFile $file
+     * @return Post
+     */
+    public function setCreatePost(Post $post, UploadedFile $file): object;
+
+    /**
+     * @param Post $post
+     * @param UploadedFile $file
+     * @return Post
+     */
+    public function setUpdatePost(Post $post, UploadedFile $file): object;
+
+    /**
+     * @param Post $post
+     * @param string $fileName
+     */
+    public function setDeletePost(Post $post, string $fileName);
 }
